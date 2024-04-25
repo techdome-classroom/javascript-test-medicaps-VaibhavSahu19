b084a9b22f79c15607f1da52566c1f27909f751a
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-04-25 13:50:21 +0530

    completed

--- program1.js
+++ program1.js
@@ -1,5 +1,28 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let ans = '';
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i; j < s.length; j++) {
+            const temp = s.substring(i, j + 1);
+            if (containsUnique(temp)) {
+                if (temp.length > ans.length) {
+                    ans = temp;
+                }
+            }
+        }
+    }
+    return ans.length;
+}
+
+function containsUnique(str) {
+    const charSet = new Set();
+    for (let char of str) {
+        if (charSet.has(char)) {
+            return false;
+        }
+        charSet.add(char);
+    }
+    return true;
 }
 
 module.exports = { longestSubstring };
--- program2.js
+++ program2.js
@@ -1,6 +1,18 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  const positiveNums = new Set();
+    for (const num of nums) {
+        if (num > 0) {
+            positiveNums.add(num);
+        }
+    }
+    let missing = 1;
+    while (positiveNums.has(missing)) {
+        missing++;
+    }
+
+    return missing;
 
 }
 
